952b3b94d7a82ffb5a242bb9272874b6bf22553f
wahhab <wahhab9969@gmail.com>
2024-12-02 20:12:58 +0700

    first commit

---
+++ src/services/storeData.js
@@ -0,0 +1,20 @@
+const { Firestore } = require("@google-cloud/firestore");
+const path = require("path");
+
+const pathKey = path.resolve("./wahhab-key.json");
+
+async function storeData(id, data) {
+  try {
+    const db = new Firestore({
+      projectId: "submissionmlgc-wahhab",
+      keyFilename: pathKey,
+    });
+
+    const predictCollection = db.collection("predictions");
+    return predictCollection.doc(id).set(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = storeData;
